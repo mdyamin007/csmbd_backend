c7f29621f8a7b05d859137627180a4de1ad2daa0
mdyamin007 <mohammadyamin80@gmail.com>
2025-03-26 21:57:09 +0600

    feat: cors issue resolved

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const { sequelize } = require('./models');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const contentRoutes = require('./routes/contentRoutes');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/v1/auth', authRoutes);
